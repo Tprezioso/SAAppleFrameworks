f6be81230a1f3963a43ce473390d48d9192bdd90
Thomas <tommyprezioso@gmail.com>
2020-12-11 17:34:50 -0500

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Thomas Prezioso Jr on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
